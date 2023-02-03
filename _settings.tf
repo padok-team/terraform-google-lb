b23b081a8472572057a37368b03159b23fc27326
Emma Dixneuf <emmadixneuf@hotmail.fr>
2023-02-03 16:47:19 +0100

    fix(versions): upgrade google provider version

--- _settings.tf
+++ _settings.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = "~> 1.0.0"
+  required_version = "~> 1.3"
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~> 3.81"
+      version = "~> 4.33"
     }
     random = {
       source  = "hashicorp/random"
--- variables.tf
+++ variables.tf
@@ -1,9 +1,3 @@
-terraform {
-  # Optional attributes and the defaults function are
-  # both experimental, so we must opt in to the experiment.
-  experiments = [module_variable_optional_attrs]
-}
-
 variable "name" {
   description = "The load balancer name."
   type        = string
